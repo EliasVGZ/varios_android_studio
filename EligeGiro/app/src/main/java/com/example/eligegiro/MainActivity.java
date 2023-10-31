442fa59b19e7fd9c1cd7a4f86b71dd4a5598e64d
elias <elias.bs2@hotmail.com>
2023-10-31 18:31:07 +0100

    saludo

--- EligeGiro/app/src/main/java/com/example/eligegiro/MainActivity.java
+++ EligeGiro/app/src/main/java/com/example/eligegiro/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RadioButton rbnTexto, rbnImagen;
     private TextView tvTexto;
     private LinearLayout ll1;
+    private EditText etGrados;
 
     private ImageView ivImagen;
     @Override
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvTexto = findViewById(R.id.tvGirando);
         ivImagen = findViewById(R.id.ivImagen);
         ll1 = findViewById(R.id.ll1);
+        etGrados = findViewById(R.id.etGrados);
 
         btnAceptar.setOnClickListener(this);
         rbnTexto.setOnClickListener(this);
@@ -42,39 +46,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvTexto.setOnClickListener(this);
         ivImagen.setOnClickListener(this);
         ll1.setOnClickListener(this);
+        etGrados.setOnClickListener(this);
 
     }
 
 
     public void onClick(View v) {
         if (v.getId() == R.id.btnAceptar) {
-            if (rbnTexto.isChecked()) {
-                ll1.setVisibility(View.GONE);
-                ivImagen.setVisibility(View.INVISIBLE);
-                tvTexto.setVisibility(View.VISIBLE);
+            String valorTexto = etGrados.getText().toString();
 
-                //GIRE 45 GRADOS AL CLICKEAR ENCIMA DEL TEXTO
-                tvTexto.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        tvTexto.setRotation(tvTexto.getRotation() + 45);
-                    }
-                });
-            } else if (rbnImagen.isChecked()) {
-                ll1.setVisibility(View.GONE);
-                tvTexto.setVisibility(View.INVISIBLE);
-                ivImagen.setVisibility(View.VISIBLE);
+            if (!valorTexto.isEmpty()) {
+                int valor = Integer.parseInt(valorTexto);//pasamos el string a int para usarlo en metodos matematicos
 
-                //GIRE 45 GRADOS AL CLICKEAR ENCIMA DEL TEXT
-                ivImagen.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ivImagen.setRotation(ivImagen.getRotation() + 45);
+                if (valor < 10 || valor > 90) {
+                    Toast.makeText(this, "Valor fuera de rango", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (rbnTexto.isChecked()) {
+                        ll1.setVisibility(View.GONE);
+                        ivImagen.setVisibility(View.INVISIBLE);
+                        tvTexto.setVisibility(View.VISIBLE);
+
+                        tvTexto.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                tvTexto.setRotation(tvTexto.getRotation() + valor);
+                            }
+                        });
+
+                    } else if (rbnImagen.isChecked()) {
+                        ll1.setVisibility(View.GONE);
+                        tvTexto.setVisibility(View.INVISIBLE);
+                        ivImagen.setVisibility(View.VISIBLE);
+
+                        ivImagen.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ivImagen.setRotation(ivImagen.getRotation() + valor);
+                            }
+                        });
                     }
-                });
+                }
+            } else {
+                Toast.makeText(this, "Debe introducir un valor", Toast.LENGTH_SHORT).show();
             }
         }
     }
+
 
 
 
